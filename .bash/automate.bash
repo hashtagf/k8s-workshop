e420ccb98c9d892c3fdeafab94917e5ef95edda1
hashtagf <flamezaxaou1@gmail.com>
2024-12-08 20:52:56 +0700

    updare:Add git configuration and push steps to automate image version updates in CD workflow

--- .bash/automate.bash
+++ .bash/automate.bash
@@ -2,5 +2,10 @@
 
 yq -i '.spec.template.spec.containers[0].image = "flhpktech/rust-demo:"+strenv(TAG_IMAGE)' ./deployments/rust-demo.yaml
 
+
+## git commit and push
+git config --global user.email "flamezaxaou1@@gmail.com"
+git config --global user.name "hashtagf"
 git add .
 git commit -m "edit image version to $TAG_IMAGE"
+git push origin main
